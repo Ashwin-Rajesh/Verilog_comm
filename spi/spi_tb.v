eeca96614813c8ab41e2b575da5698caac612053
Ashwin <aswinrajesh1@gmail.com>
2021-05-05 12:29:11 +0530

    Tested slave

--- spi/spi_tb.v
+++ spi/spi_tb.v
@@ -29,39 +29,67 @@ module spi_tb;
 
     reg r_clk = 0;
 
-    reg[p_WORD_LEN-1:0] r_m_data = 0;
-    reg r_m_dv = 0;
+    reg[p_WORD_LEN-1:0]     r_m_idata = 0;
+    reg                     r_m_dv = 0;
+    wire [p_WORD_LEN-1:0]   w_m_odata;
+
+    reg[p_WORD_LEN-1:0]     r_s_idata = 0;
+    reg                     r_s_idv = 0;
+    wire                    w_s_odv;
+    wire[p_WORD_LEN-1:0]    w_s_odata;
 
     wire w_sclk;
     wire w_mosi;
     wire w_miso;
 
-    reg r_miso = 0;
-
-    wire [p_WORD_LEN-1:0] w_m_data;
+    reg r_ss = 1;
 
     spi_master #(.p_WORD_LEN(p_WORD_LEN), .p_CLK_DIV(p_CLK_DIV)) master_dut(
         .i_clk(r_clk),
-        .i_data(r_m_data),
+        .i_data(r_m_idata),
         .i_dv(r_m_dv),
         .i_miso(w_miso),
 
         .o_sclk(w_sclk),
         .o_mosi(w_mosi),
         .o_active(w_m_active),
-        .o_data(w_m_data)
+        .o_data(w_m_odata)
     );
+
+    spi_slave #(.p_WORD_LEN(p_WORD_LEN), .p_CLK_DIV(p_CLK_DIV)) slave_dut(
+        .i_clk(r_clk),
+        .i_data(r_s_idata),
+        .i_dv(r_s_idv),
+        .i_sclk(w_sclk),
+        .i_mosi(w_mosi),
+        .i_ss(r_ss),
+
+        .o_miso(w_miso),
+        .o_dv(w_s_odv),
+        .o_data(w_s_odata)
+    );
+
 
     initial begin
         $dumpfile("spi.vcd");
         $dumpvars(0, spi_tb);
     
-        r_m_data <= 8'hEE;
+        r_m_idata <= 8'b11110000;
+        r_s_idata <= 8'b01101001;
 
-        #10
+        // Latch slave data
+        #1
+        r_s_idv     <= 1'b1;
+        #5;
+        
+        r_ss        <= 1'b0;
+        #5;
         r_m_dv      <= 1'b1;
         #5
         r_m_dv      <= 1'b0;
+
+        #300;
+        r_ss        <= 1'b1;
     end
 
     always #1 r_clk <= ~r_clk;
